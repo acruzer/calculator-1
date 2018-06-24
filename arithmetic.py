1595073bb4ab05fef523552ce853ed55a9871cae
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-24 12:12:57 -0700

    Updated add fuction

--- arithmetic.py
+++ arithmetic.py
@@ -4,7 +4,7 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return 10
+    return (num1 + num2)
 
 
 def subtract(num1, num2):
@@ -33,3 +33,4 @@ def power(num1, num2):
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
